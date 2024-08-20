1556bac2c47c3f2f0020282dc433dc4c53cf08ec
Jonathan <19248800+cowlingj@users.noreply.github.com>
2024-08-20 00:29:49 +0000

    Fetch random image on button click

--- random-photos/src/App.jsx
+++ random-photos/src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const images = import.meta.glob(['./assets/images/*.jpg'])
+
+
+function RandomImage({ index }) {
+  const [src, setSrc] = useState(null);
+
+  useEffect(() => {
+    Object.values(images)[Math.floor(Math.random() * Object.keys(images).length)]()
+      .then((module) => setSrc(module.default))
+  }, [setSrc, index]);
+
+  if (src === null) {
+    return <p>loading...</p>
+  }
+
+  return <img src={src} />
+};
+
 function App() {
   const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(null)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={() => {setIndex(Math.floor(Math.random() * Object.keys(images).length))}}
+      >
+        Random image
+      </button>
+      <br />
+      {index && <RandomImage index={index} />}
     </>
   )
 }
