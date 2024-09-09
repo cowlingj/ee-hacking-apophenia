cadb5fe91efac0dc5168e7f9a83cd206740340bf
Jonathan <19248800+cowlingj@users.noreply.github.com>
2024-09-09 18:58:59 +0000

    Fix tailwind

--- creativator/tailwind.config.js
+++ creativator/tailwind.config.js
@@ -3,7 +3,12 @@ import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {
       borderRadius: {
