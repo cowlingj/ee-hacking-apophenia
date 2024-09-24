0b05c7bcc168e34b184c5079607db7465dcc8a0c
Jonathan <19248800+cowlingj@users.noreply.github.com>
2024-09-24 14:27:20 +0000

    Random photos appearance

--- random-photos/src/App.jsx
+++ random-photos/src/App.jsx
@@ -1,38 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { Button } from "@/components/ui/button"
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { PropTypes } from "prop-types";
 
-const images = import.meta.glob(['./assets/images/*.jpg'])
+const images = import.meta.glob(["./assets/images/*.jpg"]);
 
 function ImageAsset({ imageKey }) {
   const [src, setSrc] = useState(null);
 
   useEffect(() => {
-    images[imageKey]().then((module) => setSrc(module.default))
+    images[imageKey]().then((module) => setSrc(module.default));
   }, [setSrc, imageKey]);
 
   if (src === null) {
-    return <p>loading...</p>
+    return <p>loading...</p>;
   }
 
-  return <img src={src} />
-};
+  return (
+    <div className="bg-slate-200 flex justify-center content-center">
+      <img src={src} />
+    </div>
+  );
+}
 
 function NumberButton({ n, setCount, children, max = 5, ...rest }) {
-  return <Button onClick={() => {setCount(n ?? Math.ceil(Math.random() * max))}} {...rest}>
-    { children }
-  </Button>
+  return (
+    <Button
+      onClick={() => {
+        setCount(n ?? Math.ceil(Math.random() * max));
+      }}
+      {...rest}
+    >
+      {children}
+    </Button>
+  );
 }
+
+const buttonLabels = ["One", "Two", "Three", "Four", "Five"];
+function ButtonPanel({ setCount }) {
+  const buttons = buttonLabels.map((label, i) => (
+    <NumberButton key={label} n={i + 1} setCount={setCount}>
+      {label}
+    </NumberButton>
+  ));
+
+  buttons.push(
+    <NumberButton key={"surprise"} setCount={setCount}>
+      Surprise Me!
+    </NumberButton>
+  );
+
+  return (
+    <main className="grid lg:grid-cols-3 gap-4 p-4">
+      <h1 className="col-span-full text-center">
+        How many random images would you like?
+      </h1>
+      {buttons}
+    </main>
+  );
+}
+
+ButtonPanel.propTypes = {
+  setCount: PropTypes.func.isRequired,
+};
 
 // Fisher-Yates shuffle
 // https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
 function shuffle(array) {
   const copy = [...array];
-  for (let i = copy.length - 1; i > 0; i--) { 
-    const j = Math.floor(Math.random() * (i + 1)); 
-    [copy[i], copy[j]] = [copy[j], copy[i]]; 
-  } 
-  return copy; 
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+  return copy;
 }
 
 function App() {
@@ -40,31 +79,41 @@ function App() {
   const [imageKeys, setImageKeys] = useState([]);
 
   useEffect(() => {
-    setImageKeys(shuffle(Object.keys(images)).slice(0, count) )
+    setImageKeys(shuffle(Object.keys(images)).slice(0, count));
   }, [count, setImageKeys]);
-let title = null
-
-if ( count === 0 ){
-  title = <h1>How many random images would you like?</h1>
-}
 
   return (
     <>
-      { title }
-      { count === 0 ? <NumberButton n={1} setCount={(i) => { setCount(i); setImageKeys(shuffle(Object.keys(images)).slice(0, i))}}>One</NumberButton> : null }
-      { count === 0 ? <NumberButton n={2} setCount={(i) => { setCount(i); setImageKeys(shuffle(Object.keys(images)).slice(0, i))}}>Two</NumberButton> : null }
-      { count === 0 ? <NumberButton n={3} setCount={(i) => { setCount(i); setImageKeys(shuffle(Object.keys(images)).slice(0, i))}}>Three</NumberButton> : null }
-      { count === 0 ? <NumberButton n={4} setCount={(i) => { setCount(i); setImageKeys(shuffle(Object.keys(images)).slice(0, i))}}>Four</NumberButton> : null }
-      { count === 0 ? <NumberButton n={5} setCount={(i) => { setCount(i); setImageKeys(shuffle(Object.keys(images)).slice(0, i))}}>Five</NumberButton> : null }
-      { count === 0 ? <NumberButton setCount={(i) => { setCount(i); setImageKeys(shuffle(Object.keys(images)).slice(0, i))}} >
-        Surprise me!
-      </NumberButton> : null }
+      <header className="flex-none flex gap-4 bg-primary p-4 items-baseline">
+        <h1 className="text-3xl font-bold text-white">Random Photos</h1>
+        <span className="flex-1" />
+        <img src="/ee-logo.svg" className="h-6" />
+      </header>
+      {count === 0 ? <ButtonPanel setCount={setCount} /> : null}
       <br />
-      { imageKeys.map(i => <ImageAsset key={i} imageKey={i} />) }
-      { count !== 0 ? <NumberButton n={0} setCount={(i) => { setCount(i); setImageKeys([])}} >Play Again</NumberButton> : null }
-      
+
+      {count !== 0 ? (
+        <>
+          <main className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 justify-items-strech align-items-strech">
+            {imageKeys.map((i) => (
+              <ImageAsset key={i} imageKey={i} />
+            ))}
+          </main>
+          <aside className="col-span-full flex justify-center px-4 pb-4">
+            <NumberButton
+              n={0}
+              setCount={(i) => {
+                setCount(i);
+                setImageKeys([]);
+              }}
+            >
+              Play Again
+            </NumberButton>
+          </aside>
+        </>
+      ) : null}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- random-photos/tailwind.config.js
+++ random-photos/tailwind.config.js
@@ -1,28 +1,40 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
-  prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        ee: {
+          DEFAULT: "hsl(var(--ee-blue))",
+          primary: "hsl(var(--ee-blue))",
+          secondary: {
+            DEFAULT: "hsl(var(--ee-yellow))",
+            light: "hsl(var(--ee-yellow-light))",
+          },
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -30,10 +42,6 @@ module.exports = {
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
@@ -43,35 +51,22 @@ module.exports = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
         },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [tailwindcssAnimate],
+};
