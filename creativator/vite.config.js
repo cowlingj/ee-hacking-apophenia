5d9b3b580b20def34f0104ef01f84b1d8431d8a7
Jonathan <19248800+cowlingj@users.noreply.github.com>
2024-10-12 15:38:22 +0000

    Add PWA config to creativator

--- creativator/vite.config.js
+++ creativator/vite.config.js
@@ -2,10 +2,47 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      includeAssets: ["ee-logo.svg", "favicon.png"],
+      manifest: {
+        short_name: "Creativator",
+        name: "Creativator - Hacking Apopenia",
+        description: "Generate ideas",
+        icons: [
+          {
+            src: "/ee-icon.svg",
+            type: "image/svg+xml",
+            sizes: "192x192",
+          },
+          {
+            src: "/ee-icon.svg",
+            type: "image/svg+xml",
+            sizes: "512x512",
+          },
+          {
+            src: "/ee-icon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+        ],
+        start_url: "/index.html",
+        display: "standalone",
+        theme_color: "#1795d3",
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,png,svg,jpg}"],
+        },
+        background_color: "#ffffff",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
