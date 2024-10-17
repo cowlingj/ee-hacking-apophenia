1ad5f3f281e87dd6079a1012f9cb294ee38644cc
Jonathan <19248800+cowlingj@users.noreply.github.com>
2024-10-17 00:18:48 +0000

    Add explanatory text

--- business-meerkat/src/App.jsx
+++ business-meerkat/src/App.jsx
@@ -20,19 +20,24 @@ function App() {
   const [dealt, setDealt] = useState(false);
 
   return (
-    <>
+    <div className="w-full flex gap-4 flex-col">
       <header className="flex-none flex gap-4 bg-primary p-4 items-baseline">
         <h1 className="text-3xl font-bold text-white">Business Meerkat</h1>
         <span className="flex-1" />
         <img src="/ee-logo.svg" className="h-6" />
       </header>
-      <br />
-      <main className="grid grid-cols-3 justify-items-center gap-4 p-4">
+      <main className="grid grid-cols-3 justify-items-center gap-4 p-4 flex-auto grid-rows-[max-content_1fr_max-content]">
         {dealt ? (
           <>
-            <p>Past</p>
-            <p>Present</p>
-            <p>Future</p>
+            <Button
+              onClick={() => {
+                setFlipped((cur) => [...cur].fill(false));
+                setImageKeys(shuffle(CONFIG_CARDS).slice(0, flipped.length));
+              }}
+              className="col-span-3"
+            >
+              Deal Again
+            </Button>
             {flipped.map((f, i) => (
               <button
                 key={i}
@@ -53,26 +58,46 @@ function App() {
                 )}
               </button>
             ))}
-            <Button
-              onClick={() => {
-                setFlipped((cur) => [...cur].fill(false));
-                setImageKeys(shuffle(CONFIG_CARDS).slice(0, flipped.length));
-              }}
-              className="col-span-3"
-            >
-              Deal Again
-            </Button>
           </>
         ) : (
-          <Button
-            onClick={() => setDealt(true)}
-            className="col-span-3 text-foreground bg-accent hover:bg-accent/90"
-          >
-            Deal
-          </Button>
+          <>
+            <Button
+              onClick={() => setDealt(true)}
+              className="col-span-3 bg-primary hover:bg-primary/90"
+            >
+              Deal
+            </Button>
+            <div className="justify-self-stretch grid justify-items-center items-center">
+              <div className="border rounded aspect-[421/703] max-h-[703px] w-full flex justify-center items-center">
+                <p>Past</p>
+              </div>
+            </div>
+            <div className="justify-self-stretch grid justify-items-center items-center">
+              <div className="border rounded aspect-[421/703] max-h-[703px] w-full flex justify-center items-center">
+                <p>Present</p>
+              </div>
+            </div>
+            <div className="justify-self-stretch grid justify-items-center items-center">
+              <div className="border rounded aspect-[421/703] max-h-[703px] w-full flex justify-center items-center">
+                <p>Future</p>
+              </div>
+            </div>
+          </>
         )}
+        <p>
+          This card is the perspective through which you should explain the
+          history of the problem. Where did it come from and how did it evolve?
+        </p>
+        <p>
+          This card is the current situation. Again, use the card for a
+          perspective on why the problem is a problem now.
+        </p>
+        <p>
+          This card represents the future. Use this perspective to say what
+          night happen next.
+        </p>
       </main>
-    </>
+    </div>
   );
 }
 
